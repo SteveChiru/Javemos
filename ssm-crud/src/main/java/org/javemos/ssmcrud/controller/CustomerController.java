dd78a3a07a62cad733eb73a0ec115608c93cfcbb
SteveChiru <cqr151623@163.com>
2019-01-26 16:45:05 +0800

    jQuery.ajax test ok

--- ssm-crud/src/main/java/org/javemos/ssmcrud/controller/CustomerController.java
+++ ssm-crud/src/main/java/org/javemos/ssmcrud/controller/CustomerController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description:
  */
@@ -18,12 +21,30 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @RequestMapping("/customer/list")
+    @ResponseBody
+    public List<Customer> getAllCustomers(){
+        List<Customer> customerList = new ArrayList<>();
+
+        Customer cust1 = new Customer();
+        cust1.setCust_name("小明");
+        cust1.setCust_id(1L);
+
+        Customer cust2 = new Customer();
+        cust2.setCust_name("小刚");
+        cust2.setCust_id(2L);
+
+        customerList.add(cust1);
+        customerList.add(cust2);
+
+        return customerList;
+    }
+
     @RequestMapping("/customer/edit")
     @ResponseBody
     public Customer getCustomerById(Long id) {
         logger.info("进入CustomerController中的gutCustomerById方法");
-        Customer customer = customerService.getCustomerById(14L);
-        System.out.println(customer);
+        Customer customer = customerService.getCustomerById(id);
         return customer;
     }
 
---
+++ ssm-crud/src/test/java/MapTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:
+ */
+public class MapTest {
+
+    @Test
+    public void hashMapTest(){
+        Map<Integer,String> m = new HashMap<>();
+        //增加数据
+        m.put(1,"麦迪");
+        m.put(2,"沃尔");
+        m.put(3,"艾弗森");
+
+    }
+}
