22a32ad7b8bb5b0c95535303b20891d6a6968051
SteveChiru <cqr151623@163.com>
2019-04-10 21:06:55 +0800

    echarts:compound interest data view ok

---
+++ echarts-demo/src/main/java/org/echarts/producedata/ProduceData.java
@@ -0,0 +1,26 @@
+package org.echarts.producedata;
+
+/**
+ * @Description:
+ */
+public class ProduceData {
+
+	public static void main(String[] args) {
+		//定义复利利率
+		double[] cr = {1.05,1.1,1.15,1.2,1.25,1.3};
+		int[] years = {0,5,10,15,20,25,30,35,40,45,50};
+		String data = new String();
+		for (int i=0;i<cr.length;i++){
+			for (int j=0;j<years.length;j++){
+				if (j==0){
+					data = "["+String.format("%.2f",Math.pow(cr[i],j*5)).toString()+",";
+				}else if (j==years.length - 1){
+					data += String.format("%.2f",Math.pow(cr[i],j*5)).toString()+"]";
+				}else {
+					data += String.format("%.2f",Math.pow(cr[i],j*5)).toString()+",";
+				}
+			}
+			System.out.println("复利利率是"+cr[i]+"时的数据为"+data);
+		}
+	}
+}
